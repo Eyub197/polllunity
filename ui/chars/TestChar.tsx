79d88f912a37e3040407b2543b4393bc23c502c4
Eyub197 <eyubmehmed122@gmail.com>
2024-03-09 09:28:50 +0200

    Added test charts

---
+++ ui/chars/TestChar.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { Id, Option } from "@/lib/types"
+import { getOptionsByFk } from "@/lib/utils/options"
+import { Bar } from "react-chartjs-2"
+import { Suspense } from "react"
+import 'chart.js/auto';
+
+const TestChart =  ({charData}:{ charData: Option[] } ) => {
+    const data = charData
+
+    const chartData = {
+        labels: data?.map(option => option.option_text),
+        datasets: [
+            {
+                label: "Учителка на годиниата",
+                data : data?.map(option => option.votes_count),
+                backgroundColor: "#45DFC9",
+                borderWidth: 3,
+                borderColor: "#45DFC9",
+                borderRadios: "3pt",
+                width: "40%"
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <h1>Test</h1>
+            <Suspense fallback={<p>Loading data...</p>}>
+                <Bar data={chartData}/>
+            </Suspense>
+        </div>
+    )
+
+}
+
+export default TestChart
--- ui/polls/Results/Result.tsx
+++ ui/polls/Results/Result.tsx
@@ -1,5 +1,9 @@
 import { Id } from "@/lib/types"
 import { getOptionsByFk } from "@/lib/utils/options"
+import TestChart from "@/ui/chars/TestChar"
+import 'chart.js/auto';
+import styles from "@/ui/chars/Test.module.css"
+
 
 const Result = async ( {id}:Id ) => {
     const options = await getOptionsByFk(id)
@@ -8,8 +12,11 @@ const Result = async ( {id}:Id ) => {
         <>
         {
             options!.map(option=> <p>{`${option.option_text} брой гласове ${option.votes_count} `}</p>)
-        
+            
        }
+       <div className={styles.test}>
+        <TestChart charData={options!}/>
+       </div>
 
         </>
     )
