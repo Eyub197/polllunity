101f175d46277c653deeac6cae24b83f01778ef0
Eyub197 <eyubmehmed122@gmail.com>
2024-03-30 17:14:58 +0200

    fixed deployment  issue option buttons

--- ui/options/CreateOption.tsx
+++ ui/options/CreateOption.tsx
@@ -1,43 +1,48 @@
-import { createOption, getOptions } from "@/lib/utils/options"
-import { DeleteOptionButton } from "../Buttons"
-import { EditOptionButton } from "../Buttons"
+import { createOption, deleteOption, getOptions } from "@/lib/utils/options"
+import { DeleteButtonServer, EditButton } from "../Buttons"
 
 const CreateOption = async () => {
     const options = await getOptions()
     
-    console.log(options)
     const createOptionElements = async () => {
 
         if(options?.length! > 0){
-            return (options?.map(option => 
-                <div>
+            return (options?.map(option => {
+                const deleteFunction = deleteOption.bind(null, option.id)
+                
+                return(
+                <div key={option.id}>
                     <p>{option.option_text}</p>
                     <p>{option.polls.title}</p>
-                    <DeleteOptionButton id={option.id}/>
-                    <EditOptionButton id={option.id}/>
-                </div> ))
-        }
+                    <EditButton id={option.id} toEdit="options"/>
+                    <DeleteButtonServer action={deleteFunction} id={option.id} helper={null}/>
+                </div> )
+                
+                }
+            ))}
+            
+    }    
 
-    }
-
-    return(
-        <>
-            <form action={createOption}>
-                <div>
-                    <label htmlFor="option_text">text</label>  
-                    <input type="text" id="option_text" name="option_text" />
-                </div>
-                <div>
-                    <label htmlFor="poll_id">poll id</label>  
-                    <input type="text" id="poll_id" name="poll_id" />
-                </div>
-                <button>action</button>
-            </form>
-        <>
-                {createOptionElements() || <p>Не сте създали опции</p>}
+        return(
+            <>
+                <form action={createOption}>
+                    <div>
+                        <label htmlFor="option_text">text</label>  
+                        <input type="text" id="option_text" name="option_text" />
+                    </div>
+                    <div>
+                        <label htmlFor="poll_id">poll id</label>  
+                        <input type="text" id="poll_id" name="poll_id" />
+                    </div>
+                    <button>action</button>
+                </form>
+            <>
+                    {createOptionElements() || <p>Не сте създали опции</p>}
+                </>
             </>
-        </>
-    )
+        )
 }
+
+
 
 export default CreateOption
