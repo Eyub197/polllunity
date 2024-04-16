4b55c7f2b5711b9a6c57f0320b9681049778ad7d
Eyub197 <eyubmehmed122@gmail.com>
2024-04-16 08:41:43 +0300

    started getting thinks done

--- ui/components/framerMotion/Benefits.tsx
+++ ui/components/framerMotion/Benefits.tsx
@@ -9,32 +9,59 @@ import secure from "@/public/secure.webp"
 import realTime2 from "@/public/realTime2.webp"
 
 const Benefits = () => {
+ 
     const ref = useRef(null)
     const ref2 = useRef(null)
     const ref3 = useRef(null)
-    const isInView = useInView(ref, { once: false, margin:"-300px"})
-    const isInView2 = useInView(ref2, { once: false, margin:"-300px"})
-    const isInView3 = useInView(ref3, { once: false, margin:"-200px"})
+    const isInView = useInView(ref, { once: false, margin:"-250px"})
+    const isInView2 = useInView(ref2, { once: false, margin:"-250px"})
+    const isInView3 = useInView(ref3, { once: false, margin:"-250px"})
+ 
     return (
-        <section>
+        <section className={styles.benefits}>
+            
             <motion.div
              ref={ref}
-             className={styles.benefits}
-             initial={{ opacity: 0, x: "-300vw" }}  
-             animate={{ opacity: isInView ? 1 : 0.1, x: isInView ? 0 : -100 }}
+             className={styles.benefit}
+             initial={{ opacity: 0, x: "-100vw" }}  
+             animate={{ opacity: isInView ? 1 : 0, x: isInView ? 0 : -100 }}
              transition={{ type: "spring", stiffness: 100, duration: 0.6 }}
             >
                 <Image className={styles.benefit_image} src={intuitiveDesign} alt="снимка на интуитивен дизайн" />
-                <div className={styles.bottom_part_benefit}>
-                    <h3 className={styles.benefit_title}>Интуитивен Дизайн</h3>
-                    <p className={styles.benefit_text}>Създавайте анкети с лекота благодарение  лесен за използване интерфейс<span className="pc">.</span></p>
-                </div>
+               
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{delay: 0.8, duration: "250ms"}}
+                
+                
+                className={styles.bottom_part_benefit}>
+                    <motion.h3
+                     className={styles.benefit_title}
+                     initial={{ opacity: 0 }}
+                     animate={{  opacity: isInView ? 1 : 0 }}
+                     transition={{delay: 1, duration: "250ms"}}
+                     
+                     
+                     
+                     >Интуитивен Дизайн</motion.h3>
+                    <motion.p 
+                    className={styles.benefit_text}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{delay: 1.2, duration: "250ms"}}
+                    
+                    
+                    >Създавайте анкети с лекота благодарение  лесен за използване интерфейс<span className="pc">.</span></motion.p>
+                </motion.div>
+
             </motion.div>
+            
             <motion.div
              ref={ref2}
-             className={styles.benefits}
+             className={styles.benefit}
              initial={{ opacity: 0, x: "200vw" }}
-             animate={{ opacity: isInView2 ? 1 : 0.1, x: isInView2 ? 0 : 200 }}
+             animate={{ opacity: isInView2 ? 1 : 0, x: isInView2 ? 0 : 100 }}
              transition={{ type: "spring", stiffness: 100, duration: 0.6 }}
              >
             <Image src={secure} alt="снимка на катинар"  className={styles.benefit_image} />
@@ -50,9 +77,9 @@ const Benefits = () => {
 
             <motion.div
              ref={ref3}
-             className={styles.benefits}
+             className={styles.benefit}
              initial={{ opacity: 0, x: "-200vw" }}
-             animate={{ opacity: isInView3 ? 1 : 0.1, x: isInView3 ? 0 : -100 }}
+             animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -100 }}
              transition={{ type: "spring", stiffness: 100, duration: 0.6 }}
             >
                 <Image className={styles.benefit_image} src={realTime2} alt="снимка на интуитивен дизайн" />
