7f398cef9bc439e9d5621cb4188f4ac2c658441e
Eyub197 <eyubmehmed122@gmail.com>
2024-04-22 06:11:20 +0300

    made a not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image"
+import notFoundImage from "@/public/notFoundImage.png"
+import styles from "@/app/not-found.module.css"
+import Link from "next/link"
+
 const NotFound = () => {
     return(
-        <h1> Yoo the page you were trying to accses it eaten</h1>
+        <main className={styles.main}>
+            <h1 className={styles.title}> Страницата, която търсите, сега се разследва. Това е мистерия, която дори Шерлок не може да разреши!</h1>
+            <section className={styles.container}>
+            <Image className={styles.image} src={notFoundImage} alt="Изследовате с лупа, който гледа оснавени стъпки" />
+            <Link className={styles.link} href={"/"}>Начална страница</Link>
+            </section>
+        </main>
+
     )
 }
 
